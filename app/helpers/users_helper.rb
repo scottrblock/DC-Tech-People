967ce8106e580fdd5c03d3bde7b88cf4dcd7349c
Scott Block <scottrblock@gmail.com>
2014-05-30 16:26:48 -0400

    leaders

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,4 +15,22 @@ module UsersHelper
     num_gender(gender = "female") + num_gender(gender = "male")
   end
 
+  def num_gender_who_is_leader(gender = "female")
+    num = 0
+    User.all.each do |u|
+
+      if u.gender == gender && u.probability >= 0.75 && u.follower_count >= 1000
+        puts u.name
+        num += 1
+      end
+    end
+
+    num
+
+  end
+
+  def total_leaders
+    num_gender_who_is_leader(gender = "female") + num_gender_who_is_leader(gender = "male")
+  end
+
 end
